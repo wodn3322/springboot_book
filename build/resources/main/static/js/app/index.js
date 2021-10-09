374741d273b62a2a511369c4b65bae09833c1aa1
kjw <wodn3322@gmail.com>
2021-10-09 16:08:35 +0900

    google clould platform이용하여 OAuth을 이용한 로그인 기능 구현

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -1,51 +1,69 @@
 var main = {
-    init : function (){
+    init: function () {
         var _this = this;
-        $('#btn-save').on('click',function (){
+        $('#btn-save').on('click', function () {
             _this.save();
         });
-        $('#btn-update').on('click',function (){
+        $('#btn-update').on('click', function () {
             _this.update();
-        })
+        });
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
-    save : function (){
+    save: function () {
         var data = {
-            title : $('#title').val(),
-            author : $('#author').val(),
-            content : $("#content").val()
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $("#content").val()
         };
 
         $.ajax({
             type: 'POST',
-            url:'/api/v1/posts',
-            dataType : 'json',
-            contentType : 'application/json; charset=utf-8',
-            data : JSON.stringify(data)
-        }).done(function (){
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
             alert('글이 등록되었습니다.');
-            window.location.href='/';
-        }).fail(function (error){
+            window.location.href = '/';
+        }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     },
-    update : function (){
+    update: function () {
         var data = {
-            title : $('#title').val(),
-            content : $("#content").val()
+            title: $('#title').val(),
+            content: $("#content").val()
         };
 
         var id = $('#id').val();
 
         $.ajax({
             type: 'PUT',
-            url:'/api/v1/posts/'+id,
-            dataType : 'json',
-            contentType : 'application/json; charset=utf-8',
-            data : JSON.stringify(data)
-        }).done(function (){
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
             alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete: function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type:'delete',
+            url:'/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function () {
+            alert('글이 삭제되었습니다.');
             window.location.href='/';
-        }).fail(function (error){
+        }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     }
---
+++ src/main/java/com/jaewoo/book/springboot/config/auth/SessionUser.java
@@ -0,0 +1,19 @@
+package com.jaewoo.book.springboot.config.auth;
+
+import com.jaewoo.book.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user){
+        this.name=user.getName();
+        this.email=user.getEmail();
+        this.picture=user.getPicture();
+    }
+}
